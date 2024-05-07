1bd6e338f0f906086a84004e5d29dd62b61991f8
Mangalam Raj <mango.26june@gmail.com>
2024-05-07 17:34:53 +0530

    button fix

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -18,8 +18,8 @@ import { useKeenSlider } from "keen-slider/react";
 
 const Navbar = () => {
   return (
-    <div className="sticky top-0 z-50  h-16 w-full items-center backdrop-blur shadow-sm  px-2 flex justify-between bg-black">
-      <div className="container font-semibold text-xl md:text-2xl ">
+    <div className="sticky top-0 z-50 h-16 w-full items-center backdrop-blur shadow-sm px-2 flex justify-between bg-black">
+      <div className="container font-semibold text-xl md:text-2xl">
         <Image src="/Logo3.svg" alt="" width={100} height={100}></Image>
       </div>
       {/* <NavigationMenu className="bg-black">
@@ -73,13 +73,15 @@ const Navbar = () => {
           </NavigationMenuList> */}
       {/* </NavigationMenu> */}
       {/* <ModeToggle /> */}
-      <div className="">
-        <Button className="container rounded-3xl text-white bg-[#135de8] hover:bg-white hover:text-black font-semibold text-sm ">
-          Download
-        </Button>
+      <div className="container flex items-center">
+        <div className="ml-auto">
+          <Button className="rounded-3xl text-white bg-[#135de8] hover:bg-white hover:text-black font-semibold text-sm">
+            Download
+          </Button>
+        </div>
       </div>
-      s
     </div>
   );
 };
+
 export default Navbar;
