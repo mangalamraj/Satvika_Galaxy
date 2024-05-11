bc9e704f1d1b1485a5c6b013000fd9a0c72e0d0b
Mangalam Raj <mango.26june@gmail.com>
2024-05-11 20:22:36 +0530

    hero section partial done

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,49 +1,67 @@
-import Slider from "@/components/slider";
 import Image from "next/image";
-import "./home.css";
-// import { particlesJS } from "particles.js";
+import "./home.css"; // Assuming this contains additional styles
+
 export default function Home() {
-  // particlesJS.load("particles-js", "assets/particles-config.json", function () {
-  //   console.log("callback - particles.js config loaded");
-  // });
   return (
-    <main className="bg-black flex flex-col justify-center items-center pt-20 pb-10 background">
-      {" "}
-      <div className="md:w-1/6 w-52 pb-7">
-        <Image
-          src="./gALAXY.svg"
-          width={0}
-          height={0}
-          className="w-full h-full"
-          alt=""
-        ></Image>{" "}
+    <main className="relative bg-black h-[88vh] flex flex-col 2xl:pt-20 pt-10 items-center background overflow-hidden">
+      <div className="w-full flex flex-col justify-between h-full">
+        <div className="flex flex-col gap-6 w-full items-center">
+          <div className="w-66">
+            <Image
+              src="/gALAXY.svg"
+              width={0}
+              height={0}
+              className="w-full h-full"
+              alt=""
+            />
+          </div>
+
+          <div className="md:w-40 w-40">
+            <Image
+              src="/LIVING .svg"
+              width={0}
+              height={0}
+              className="w-full h-full"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div className="w-full md:pb-4 pb-8 -z-1">
+          <div className="container md:w-[100%] w-full md:pb-7 pb-3 opacity-75">
+            <Image
+              src="/1.svg"
+              width={0}
+              height={0}
+              className="w-full h-full"
+              alt=""
+            />
+          </div>
+          <div className="container md:w-[95%] w-full">
+            <Image
+              src="/2.svg"
+              width={0}
+              height={0}
+              className="w-full h-full pb-7"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
-      <div className="md:w-40 w-40 pb-12">
-        <Image
-          src="./LIVING .svg"
+      <div className="absolute  -bottom-36 md:-bottom-44 w-full overflow-hidden">
+        <img
+          src="/p11.png"
           width={0}
           height={0}
-          className="w-full h-full"
+          className="mx-auto 2xl:w-[760px] w-[640px]"
           alt=""
-        ></Image>
+        />
       </div>
-      <div className="container md:w-[90%] w-full md:pb-7 pb-3 opacity-75">
-        <Image
-          src="./1.svg"
-          width={0}
-          height={0}
-          className="w-full h-full"
-          alt=""
-        ></Image>
+      <div className="absolute hidden md:block md:-bottom-20 left-0 w-2/3  overflow-hidden">
+        <img src="/cloud.png" className="w-[400px] m-auto" />
       </div>
-      <div className="container md:w-[88%] w-full">
-        <Image
-          src="./2.svg"
-          width={0}
-          height={0}
-          className="w-full h-full pb-7"
-          alt=""
-        ></Image>
+      <div className="absolute hidden md:block md:bottom-16 right-0 w-2/3  overflow-hidden">
+        <img src="/cloud.png" className="w-[400px] m-auto" />
       </div>
     </main>
   );
