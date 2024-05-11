86a37d600d75d21f59a3f70e43aba4046e1704be
Mangalam Raj <mango.26june@gmail.com>
2024-05-11 23:51:32 +0530

    second last component made

---
+++ client/src/components/satvikaCTA/cta.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+const CTA = () => {
+  return (
+    <main className="relative bg-black text-white py-20 ">
+      <div className="container flex flex-col items-center">
+        <Image
+          width={200}
+          height={200}
+          alt=""
+          src="/Logo3.svg"
+          className="pb-10 md:pb-14"
+        />
+        <div className="container absolute top-40 md:w-[100%] w-full md:pb-7 pb-3 opacity-75">
+          <Image
+            src="/1.svg"
+            width={0}
+            height={0}
+            className="w-full h-full"
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="font-semibold text-3xl md:text-5xl text-white md:text-center md:w-[80%] m-auto">
+            benchmark development that exceeds expectations
+          </div>
+          <div className="flex flex-col gap-2 md:text-center">
+            <div className="flex font-semibold text-[#86868b] flex-col gap-2 text-lg md:text-xl">
+              from SATVIKA Group
+            </div>
+            <div className="text-sm font-medium ">
+              Know more about the developers.{" "}
+              <p className="inline text-blue-600">Satvikagroup.in</p>
+            </div>
+          </div>
+          <div className="md:w-[80%] md:m-auto">
+            <img src="/fistbump.png" className="w-full" />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default CTA;
