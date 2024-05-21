04498b51f82e38b6b29a7ef69af48a6e05e9c7bf
Mangalam Raj <mango.26june@gmail.com>
2024-05-21 15:06:18 +0530

    video add

--- client/src/components/videoComponent/videoComponent.tsx
+++ client/src/components/videoComponent/videoComponent.tsx
@@ -1,31 +1,23 @@
 import Image from "next/image";
+import Video from "next-video";
+import video from "/videos/v1.m4v";
 const VideoComponent = () => {
   return (
-    <div className="relative bg-black">
-      <video src="/SatvikaGalaxy-1.mp4" className="m-auto"></video>
+    <div className=" relative bg-black">
+      <Video src={video} className="m-auto"></Video>
       <div className="relative md:w-[68%] m-auto">
         <div className="flex text-white">
           <Image
-            src="/youtube.png"
+            src="/youtube.svg"
             alt=""
-            className="absolute bottom-14 left-0 p-4 w-[70px]"
+            className="absolute bottom-14 left-0 p-4 w-[100px]"
             width={70}
             height={70}
           />
-          <div className="absolute bottom-12  left-12 p-4 my-3 text-xl font-semibold">
-            YouTube
-          </div>
-          <div className="text-white absolute bottom-3 left-0 p-4 my-3 font-semibold">
+          <div className="text-white absolute text-sm bottom-3 left-0 p-4 my-3 font-semibold">
             Galaxy Walkthrough
           </div>
         </div>
-        <Image
-          src="/play-button.png"
-          alt=""
-          className="absolute bottom-4 right-0 p-4"
-          width={70}
-          height={70}
-        />
       </div>
     </div>
   );
